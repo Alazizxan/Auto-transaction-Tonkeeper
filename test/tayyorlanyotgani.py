d97134b8d3fd57071b071b8c44216d5b07f3f86a
Alazizxan <azikcanpapagul@gmail.com>
2024-12-08 15:30:07 +0500

    tayyor erorrlarni kamaytirish ishlaydi

--- test/tayyorlanyotgani.py
+++ test/tayyorlanyotgani.py
@@ -57,11 +57,11 @@ try:
 
     # Qiymatni tekshirib turuvchi loop
     while True:
-        # Qiymatni o'qib olish
-        ton_value_element = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-RXieO hbKeZp']//span[@class='sc-jSguLX egBbuV']")))
-        ton_value = ton_value_element.text.replace(",", ".")  # Qiymatni float uchun mos formatga o'zgartiramiz
-
         try:
+            # Qiymatni o'qib olish
+            ton_value_element = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='sc-RXieO hbKeZp']//span[@class='sc-jSguLX egBbuV']")))
+            ton_value = ton_value_element.text.replace(",", ".")  # Qiymatni float uchun mos formatga o'zgartiramiz
+
             ton_value_float = float(ton_value)
             print(f"Hozirgi qiymat: {ton_value_float}")
 
@@ -71,9 +71,8 @@ try:
                 send_button = driver.find_element(By.XPATH, "//button[contains(@class, 'sc-bXDltw') and contains(., 'Send')]")
                 driver.execute_script("arguments[0].click();", send_button)
 
-
                 textarea = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.TAG_NAME, "textarea"))
+                    EC.presence_of_element_located((By.TAG_NAME, "textarea"))
                 )
                 textarea.send_keys('UQBjnfy1nGfJFZchNUj7hHEWy1hi-nEvm0CZvP2oNRv5dOF8')
 
@@ -88,7 +87,6 @@ try:
 
                 max_span = driver.find_element(By.XPATH, "//span[text()='MAX']")
                 driver.execute_script("arguments[0].click();", max_span)
-                
 
                 # `Continue` tugmasini topish va bosish
                 continue_button = driver.find_element(By.XPATH, "//button[text()='Continue']")
@@ -113,14 +111,15 @@ try:
 
                 # 'Confirm' tugmasini bosish
                 confirm_button.click()
-                
+
             else:
                 print("Qiymat 0.0001 dan kichik.")
 
-        except ValueError:
-            print("Qiymatni floatga aylantirishda xatolik yuz berdi!")
-
-        time.sleep(3)  # Har 5 soniyada tekshiruvni takrorlash
+        except Exception as e:
+            print(f"Xatolik yuz berdi: {e}")
+        
+        driver.refresh()
+        time.sleep(3)  # Har 3 soniyada tekshiruvni takrorlash
 
 except Exception as e:
     print(f"Xatolik yuz berdi: {e}")
